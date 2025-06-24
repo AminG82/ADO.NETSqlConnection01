849c776d3f4fd1cd05a1af78559488f911412a38
Amin Ghasemi <mmnghasemi@gmail.com>
2025-06-24 14:44:02 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+
+SqlConnection connectionTest = new SqlConnection("""
+    
+    Server=localhost;Database=TestDB;
+    User Id=sa;Password=amin5123;
+    
+    """);
+    
+
+SqlCommand insertCommand = new SqlCommand("""
+    
+    INSERT INTO Person (Name,LastName, Age)
+    VALUES ('Rebecca','DeWinter', 23)
+    
+    """, connectionTest);
+
+connectionTest.Open();
+Console.WriteLine(insertCommand.ExecuteNonQuery());
+
+connectionTest.Close();
+
+Console.ReadKey();
